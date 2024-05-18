cf57b93f071d18d9350d5a2c85a61ba0fde06daf
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-05-18 12:11:21 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,14 @@
+// Question: 1
+var a="1" ; b="2" ; c="3"
+console.log(a+b+c)
+//Question: 2
+// var age ; Age ; abc ; quality ; name
+// var 1Hello ; 2_name ; 3_quality ; 4_Age ; 3_product
+
+//QUestion: 3
+var A = "Rules for naming JS variable" ; num = "numbers" ; variable = "$" ; under = "_" ; na = "$my_1stVariable"
+document.write("<h2>"+A+"</h2>")
+document.write("</br>"+"Variable names can only contain , "+num+" , "+variable+" and "+under+" . For example :"+na)
+document.write("</br>"+"Variable must begin with the letter,"+variable+" or "+under+" . For example : $name, _name or name")
+document.write("</br>"+"Variable names are case sensitive")
+document.write("</br>"+"Variable names should not be JS keywords")
